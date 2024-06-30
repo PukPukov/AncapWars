6e4683da87d3d4f5eaeac8168138c315b63d6451
PukPukov <kvasov.les@gmail.com>
2024-06-30 05:13:00 +0500

    allow trident enchantments; refactor; update to 1.20.6 api

--- src/main/java/ru/ancap/states/wars/war/process/restriction/tester/item/EnchantmentTester.java
+++ src/main/java/ru/ancap/states/wars/war/process/restriction/tester/item/EnchantmentTester.java
@@ -1,12 +1,32 @@
 package ru.ancap.states.wars.war.process.restriction.tester.item;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Accessors(fluent = true) @Getter
+@ToString @EqualsAndHashCode
 public class EnchantmentTester implements ItemStackPredicate {
 
+    private final Map<Enchantment, Integer> allowedEnchantments;
+    
     @Override
     public boolean test(ItemStack itemStack) {
-        return itemStack.getEnchantments().isEmpty();
+        Map<Enchantment, Integer> enchantments = itemStack.getEnchantments();
+        for (var entry : enchantments.entrySet()) {
+            @Nullable Integer allowState = this.allowedEnchantments.get(entry.getKey());
+            if (allowState == null) return false;
+            if (allowState < entry.getValue()) return false;
+        }
+        return true;
     }
 
 }
